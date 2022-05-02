14ca40e9d0ac4f7c5e2f338de851cd6733c9a770
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-05-02 00:59:49 -0600

    added read me and colored the input lines

--- Develop/script.js
+++ Develop/script.js
@@ -1,53 +1,57 @@
 var currentDate = moment().format('MM/DD/YYYY');
-var input1 = document.getElementById("input-1");
-var input2 = document.getElementById("input-2");
-var input3 = document.getElementById("input-3");
-var input4 = document.getElementById("input-4");
-var input5 = document.getElementById("input-5");
-var input6 = document.getElementById("input-6");
-var input7 = document.getElementById("input-7");
-var input8 = document.getElementById("input-8");
-var input9 = document.getElementById("input-9");
-$("#currentDay").append(currentDate);
+var input1 = document.getElementById("9");
+var input2 = document.getElementById("10");
+var input3 = document.getElementById("11");
+var input4 = document.getElementById("12");
+var input5 = document.getElementById("13");
+var input6 = document.getElementById("14");
+var input7 = document.getElementById("15");
+var input8 = document.getElementById("16");
+var input9 = document.getElementById("17");
+var currentTime = moment().hour();
+//Displays current date
+$("#currentDay").append("Today's date is " + currentDate);
 
-
+//saves item to local storage based on the button to input connection
 $("#btn-1").on("click", function () {
-    localStorage.setItem("task-input-1", input1.value);
+    localStorage.setItem("task-9", input1.value);
 });
 
 $("#btn-2").on("click", function () {
-    localStorage.setItem("task-input-2", input2.value);
+    localStorage.setItem("task-10", input2.value);
 });
 
 $("#btn-3").on("click", function () {
-    localStorage.setItem("task-input-3", input3.value);
+    localStorage.setItem("task-11", input3.value);
 });
 
 $("#btn-4").on("click", function () {
-    localStorage.setItem("task-input-4", input4.value);
+    localStorage.setItem("task-12", input4.value);
 });
 
 $("#btn-5").on("click", function () {
-    localStorage.setItem("task-input-5", input5.value);
+    localStorage.setItem("task-13", input5.value);
 });
 
 $("#btn-6").on("click", function () {
-    localStorage.setItem("task-input-6", input6.value);
+    localStorage.setItem("task-14", input6.value);
 });
 
 $("#btn-7").on("click", function () {
-    localStorage.setItem("task-input-7", input7.value);
+    localStorage.setItem("task-15", input7.value);
 });
 
 $("#btn-8").on("click", function () {
-    localStorage.setItem("task-input-8", input8.value);
+    localStorage.setItem("task-16", input8.value);
 });
 
 $("#btn-9").on("click", function () {
-    localStorage.setItem("task-input-9", input9.value);
+    localStorage.setItem("task-17", input9.value);
 });
 
+
 var getLocalStorage = function() {
+    //gets the current hour
     const d = new Date()
     const hour = d.toLocaleTimeString('en-US', {
         // en-US can be set to 'default' to use user's browser settings
@@ -55,16 +59,27 @@ var getLocalStorage = function() {
         
       });
     console.log(hour);
-    const timeArr = [9, 10, 11, 12, 1, 2, 3, 4, 5,]
+    //gets the local storage from array and appends it to page
+    const timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17,]
     for (let i = 1; i < 10; i++) { 
-        var task = localStorage.getItem(`task-input-${i}`)
+        var task = localStorage.getItem(`task-${i}`)
         if (task) {
-            console.log(task);
-            var input = $(`#input-${i}`)
+            var input = $(`#${i}`)
             input.val(task);
-            console.log(input)
         };
       };
+
+      $("input").each(function () {
+        var time = $(this).attr("id");
+      
+        if (time < currentTime) {
+          $(this).addClass("past-time");
+        } else if (time > currentTime) {
+          $(this).addClass("future-time");
+        } else {
+          $(this).addClass("present-time");
+        }
+      });
 };
 
 getLocalStorage();
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Work Day Scheduler Starter Code
+# Horiseon Landing Page/ Horiseon-refractor
+
+## My motivation behind this project was to make an day planner that had the current date on it as well as being able to track your saved items throughout the day without having to keep it open.
+
+## [Installation] (#installation) In order to install my program: Step1-create a new directory on local computer. Step2-Go to my GitHub profile @Bvatuvei. Step3-Go to my repositories and select "Horiseon-Refactor". Step4-Click on code and then copy ssh or https. Step5- Open terminal and type "git clone <"your previously copied ssh/https">.
+
+## [link] (link) [code-link]https://github.com/Bvatuvei/Horiseon-Refactor, [website-link]https://bvatuvei.github.io/Horiseon-Refactor/
+
+## [Screenshot of the page] )
+! [top half of page](assets/images/Screen%20Shot%202022-03-15%20at%2012.52.12%20PM.png)
+! [bottom half of page](assets/images/Screen%20Shot%202022-03-15%20at%2012.52.21%20PM.png)
